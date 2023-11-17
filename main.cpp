c9530407fa34a9e515f3113d2cfcff883f479f39
Yoru <58902997+Y0r5@users.noreply.github.com>
2023-11-17 15:38:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 #include <filesystem>;
 #include <vector>;
 
-std::string injection_code = "module.exports = require('./core.asar');"; // insert injection code here NOTE: make sure to keep the module.export part since thats what discord uses to run the dekstop core client
-std::vector<std::string> dir_names_node = {"Discord", "DiscordCanary", "DiscordPTB", "DiscordDevelopment"}; // supports most discord application TODO: BetterDiscord (Idk if it works on it)
+std::string injection_code = "module.exports = require('./core.asar');"; // (EN) insert injection code here NOTE: make sure to keep the module.export part since thats what discord uses to run the dekstop core client (ES) // inserta el código de inyección aquí NOTA: asegúrate de conservar la parte module.export ya que eso es lo que usa Discord para ejecutar el cliente principal de escritorio
+std::vector<std::string> dir_names_node = {"Discord", "DiscordCanary", "DiscordPTB", "DiscordDevelopment"}; // (EN) supports most discord application TODO: BetterDiscord (Idk if it works on it) // (ES) admite la mayoría de las aplicaciones de discord TODO: BetterDiscord (no sé si funciona en él)
 
-std::string get_local_app_data() { // grab local app data return it as a string to use it in fs
+std::string get_local_app_data() { // (EN) grab local app data return it as a string to use it in fs (ES) // toma los datos de la aplicación local y los devuelve como una cadena para usarlos en fs
     const char* app_data = std::getenv("LOCALAPPDATA");
     if (app_data == nullptr) {
         return "";
@@ -16,7 +16,7 @@ std::string get_local_app_data() { // grab local app data return it as a string 
     return std::string(app_data);
 };
 
-void edit_discord_file(const std::string& dir_name) { // this function goes through the app data folder while looking through the directory you give it for the index.js file to inject the given code in it
+void edit_discord_file(const std::string& dir_name) { // (EN) this function goes through the app data folder while looking through the directory you give it for the index.js file to inject the given code in it (ES) // esta función recorre la carpeta de datos de la aplicación mientras busca en el directorio que le proporciona el archivo index.js para inyectar el código dado en él
     std::filesystem::path file_path = std::filesystem::path(get_local_app_data()) / dir_name;
 
     if (!std::filesystem::is_directory(file_path)) {
@@ -38,7 +38,7 @@ void edit_discord_file(const std::string& dir_name) { // this function goes thro
 }
 
 int main() {
-    for (const std::string dir_name: dir_names_node) { // self explanotory loop for all the Discord types NOTE: the index.js file will only run every time discord opens.
+    for (const std::string dir_name: dir_names_node) { // (EN) self explanotory loop for all the Discord types NOTE: the index.js file will only run every time discord opens. // (ES) bucle autoexplicativo para todos los tipos de Discord NOTA: el archivo index.js solo se ejecutará cada vez que se abra Discord.
         edit_discord_file(dir_name);
     };
     return 0;
